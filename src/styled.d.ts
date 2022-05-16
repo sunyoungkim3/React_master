1d1f36abf2be402bae066ce6c02d1d539aa26d17
Sunny <kimsyng1120@gmail.com>
2022-05-16 21:11:58 +0900

    3.6 Themes

---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import { } from 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme{
+        textColor: string;
+        bgColor:string;
+        btnColor:string;
+    } // extends the global DefaultTheme with our ThemeType.
+}
